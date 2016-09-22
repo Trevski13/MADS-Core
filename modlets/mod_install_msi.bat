9b770390c4fca6a52b51e58b121cb8d0401195d1
Trevor Buttrey <4508b955@opayq.com>
2016-09-22 13:29:11 -0400

    Update msi install
    Added description, edited input flags, and altered to use mod_error

--- modlets/mod_install_msi.bat
+++ modlets/mod_install_msi.bat
@@ -1,7 +1,11 @@
 @echo off
+
+REM This modlet installs a program from an MSI file
+
 setlocal enabledelayedexpansion
 call mod_flag_parsing %*
-call mod_flag_check /type string /flag file
+call mod_flag_check /type file /flag file
+call mod_flag_check /type dir /flag directory /defaultValue .\
 call mod_flag_check /type string /flag name
 call mod_flag_check /type boolean /flag passive /defaultValue true
 call mod_flag_check /type string /flag args /defaultValue " "
@@ -16,24 +20,7 @@ if "[%debug%]"=="[true]" echo DEBUG: done Setting Passive Status
 call mod_echo Installing %flag_name%...
 call mod_log Installing %flag_name% via %flag_file%
 if "[%debug%]"=="[true]" echo DEBUG: Installing...
-start "" /wait msiexec /i %flag_file% %passive% %flag_args%
-if ERRORLEVEL 1 (
-	if %errorlevel% NEQ 3010 (
-		if %errorlevel% NEQ 1605 (
-			call mod_tee  Error: %errorlevel% /color 0C
-			call mod_msi_error_lookup %errorlevel%
-			set /a errorct+=1
-			call mod_pause
-		) else (
-			call mod_tee  Error: %errorlevel% /color 0E
-			call mod_msi_error_lookup %errorlevel%
-		)
-	) else (
-		call mod_msi_error_lookup %errorlevel%
-		call mod_tee Installed /color 0A
-	)
-) else (
-	call mod_msi_error_lookup %errorlevel%
-	call mod_tee Installed /color 0A
-)
+start "" /wait msiexec /i %flag_directory%%flag_file% %passive% %flag_args%
+call mod_error /error %errorlevel% /alternate-successes "3010,1605"
 endlocal
+exit /b
